47f57140bd2218a3da861611b9499966aeb593d7
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-03-07 18:21:44 +0530

    fix the route bug

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,6 @@ export const api = axios.create({
   baseURL:
     import.meta.env.BACKEND_API_URL ||
     "https://pro-crm-backend.onrender.com/api/crm",
-  // "http://localhost:5000/api/crm",
 });
 
 // Attach token automatically
