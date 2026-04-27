1962f9659bf80373f909533d96b106ad9e3b3c4d
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2026-04-27 17:05:21 +0530

    update v1.4.5

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//  this the main service for making API calls to the backend. It is configured with the base URL of the backend API and automatically attaches the access token from localStorage to each request if it exists. This allows for seamless authentication when making requests to protected endpoints.
 export const api = axios.create({
   baseURL:
     import.meta.env.BACKEND_API_URL ||
